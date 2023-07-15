dfb2cfc957f6b8229a0d2a260b19d1e656bcb1ad
wdgstl <wdgstl@gmail.com>
2023-07-15 10:31:28 -0500

    Create measure_lesion.py

---
+++ measure_lesion.py
@@ -0,0 +1,7 @@
+#preprocess hist image (resize w aspect ratio)
+
+#get UNET inference (model.predict) to get segementation
+
+#pass mask into post_processing to get lesion measurements
+
+#ouput mask labeled with measurements
