fed51bce3eea3caf0894f1a50682886ce42bdf1a
wdgstl <wdgstl@gmail.com>
2024-07-28 21:40:32 +0000

    imagej to reqs

--- src/data_prepreprocessing.py
+++ src/data_prepreprocessing.py
@@ -1,6 +1,18 @@
 #Use ImageJ to get masks and measurements from raw histology images and segmentations
-import imagej as pyj
+import imagej
 
+ij = imagej.init('sc.fiji:fiji')
 
+convert_czi_to_tif = """
 print(2)
+"""
 
+
+get_mask_script = """
+
+
+"""
+
+result = ij.py.run_script("Groovy", convert_czi_to_tif)
+
+print(result)
